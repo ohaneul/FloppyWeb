0ad9d175304a50295c97df9594a129b949cd6f4a
Haneul <77052813+ohaneul@users.noreply.github.com>
2024-11-27 11:06:26 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
     },
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-dom'],
+    testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)']
 }; 
